ad66832f80b9b40a8e01d963ee9fa4309e49873a
Shrestha <234707564+shrestha-01@users.noreply.github.com>
2026-09-22 08:13:25 +0545

    rewriting the tests

--- Learn C/Practice_01.c
+++ Learn C/Practice_01.c
@@ -2,6 +2,25 @@
 
 int main(){
     printf("Hello World\n");
+    return 0;
+
+    //Checking after return
+
     printf("Will I be printed ???");
+
+}
+
+//checking another function except main()
+int shrestha(){
+    printf("does it works....nah!!!");
+    printf("Because not called");
     return 0;
 }
+
+// checking double return 
+
+// int main(){
+//     printf("Hello World\n");
+//     return 0;
+//     // printf("Will I be printed ???");
+// }
