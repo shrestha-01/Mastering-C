25257bc9dd0a64ccd8748dd5c8645843688c9bd6
Shrestha <234707564+shrestha-01@users.noreply.github.com>
2026-09-23 07:54:13 +0545

    Learned Variables naming rules

---
+++ Chapter 1/02_Variables_rules.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    // naming rules 
+    // First character must be an alphabet or underscore(_).
+    // No commas, blanks are allowed.
+    // No special symbol other than (_) allowed.
+    // Varibales names are case sensitive.
+    int a;
+    int s_shrestha;
+    int shrestha;
+    int Shrestha;
+
+    return 0;
+}
