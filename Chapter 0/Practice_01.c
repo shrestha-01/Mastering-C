6d83899ab6258759a63d7c0642c3bfcef012484b
Shrestha <234707564+shrestha-01@users.noreply.github.com>
2026-09-23 07:22:54 +0545

    Starting variables lesson

---
+++ Chapter 0/Practice_01.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(){
+    printf("Hello World\n");
+    return 0;
+
+    //Checking after return
+
+    printf("Will I be printed ??? NO");
+
+}
+
+//checking another function except main()
+int shrestha(){
+    printf("does it works....nah!!!");
+    printf("Because not called");
+    return 0;
+}
+
+// checking double return 
+
+// int main(){
+//     printf("Hello World\n");
+//     return 0;
+//     // printf("Will I be printed ???");
+// }
