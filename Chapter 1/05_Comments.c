293701aca788f029872a25df240f733eba3e55f1
Shrestha <234707564+shrestha-01@users.noreply.github.com>
2026-09-26 19:52:32 +0545

    Wrote some basic commenting code

---
+++ Chapter 1/05_Comments.c
@@ -0,0 +1,13 @@
+/* Author -> Shrestha */
+#include<stdio.h>
+int main(){
+    // This is a single line comment
+    /*This is 
+    Multiline 
+    Comment
+    */
+   // /* and */ are also know as comment delimiters
+   // Whatever is written in comments , either it is single line or multi lines inside the comment delimiters are not compiled by the compiler. Also they are only not printed. They are written as notes (usually)
+    printf("Hello World from shrestha.");
+    return 0;
+}
